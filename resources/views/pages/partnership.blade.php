09d004f3335edaec2c52b14d40a1088e2687f0b5
SoffiaNoor <soffianaisyah0@gmail.com>
2023-02-04 09:02:18 +0700

    *update partnership

--- resources/views/pages/partnership.blade.php
+++ resources/views/pages/partnership.blade.php
@@ -15,25 +15,28 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12" style="margin-bottom:3rem; margin-top:3rem">
-            <h4>Charity &amp; Industry Partnership</h4>
-            <div class="row">
-                <div class="col-sm-5" style="margin-top:50px;margin-bottom:100px;background-color:#ececec">
-                    <img style="width:104%; margin-left:-10px;height:fit-content" src="assets/images/partnership.jpg">
-                    <p style="font-size:12pt;margin-right:0px;padding:15px 15px 15px 15px; text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in neque venenatis, tincidunt libero et, porta lectus. Nam dictum ultrices vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce semper non tellus a mollis. In ultricies est sed maximus lobortis. Nam tellus magna, aliquam at nisl ac, malesuada molestie ante. Sed quis pretium dolor. Vivamus luctus velit erat, at placerat magna dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque lacinia quam vel sapien lobortis, sit amet vehicula lectus ultricies.</p>
+<div class="container" style="margin-top:3rem;margin-bottom:3rem">
+    <h4>Charity &amp; Industry Partnership</h4>
+    <div class="row row-cols-1 row-cols-sm-2" style="margin-top:3rem">
+        <div class="col">
+            <div class="card shadow-sm" style="border:transparent">
+                <img class="card-img-top" width="100%" height="100%" src="assets/images/partnership.jpg" alt="Card image cap" focusable="false">
+                <div class="card-body" style="background-color: #ececec">
+                    <p class="card-text" style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in neque venenatis, tincidunt libero et, porta lectus. Nam dictum ultrices vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce semper non tellus a mollis. In ultricies est sed maximus lobortis. Nam tellus magna, aliquam at nisl ac, malesuada molestie ante. Sed quis pretium dolor. Vivamus luctus velit erat, at placerat magna dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque lacinia quam vel sapien lobortis, sit amet vehicula lectus ultricies.</p>
                 </div>
-                <div class="col-sm-2">
-                </div>
-                <div class="col-sm-5" style="margin-top:50px;margin-bottom:100px;background-color:#ececec">
-                    <img style="width:104%; margin-left:-10px;height:fit-content" src="assets/images/partnership.jpg">
-                    <p style="font-size:12pt;margin-right:0px;padding:15px 15px 15px 15px; text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in neque venenatis, tincidunt libero et, porta lectus. Nam dictum ultrices vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce semper non tellus a mollis. In ultricies est sed maximus lobortis. Nam tellus magna, aliquam at nisl ac, malesuada molestie ante. Sed quis pretium dolor. Vivamus luctus velit erat, at placerat magna dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque lacinia quam vel sapien lobortis, sit amet vehicula lectus ultricies.</p>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card shadow-sm" style="border:transparent">
+                <img class="card-img-top" width="100%" height="100%" src="assets/images/partnership.jpg" alt="Card image cap" focusable="false">
+                <div class="card-body" style="background-color: #ececec">
+                    <p class="card-text" style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in neque venenatis, tincidunt libero et, porta lectus. Nam dictum ultrices vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce semper non tellus a mollis. In ultricies est sed maximus lobortis. Nam tellus magna, aliquam at nisl ac, malesuada molestie ante. Sed quis pretium dolor. Vivamus luctus velit erat, at placerat magna dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque lacinia quam vel sapien lobortis, sit amet vehicula lectus ultricies.</p>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 
 @endsection
 
